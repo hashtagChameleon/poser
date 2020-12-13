8c63b03b667dce39d0f740eb6186f99b9d5f1056
Shai Levi <levishai.g@gmail.com>
2020-12-13 21:29:35 +0000

    combine detectron and use video file

--- poser_demo.py
+++ poser_demo.py
@@ -1,74 +1,108 @@
 #!/usr/bin/env python3
 
 import cv2
-import numpy
+import numpy as np
 import json
 import requests
 import pickle
 import codecs
+import argparse
 from functools import singledispatch
 from datetime import datetime
 
+from detectron_demo import Detectron
 from posenet_demo import Posenet
 
 joints_name2 = ('Head_top', 'Thorax', 'rightShoulder', 'rightElbow', 'rightWrist', 'leftShoulder', 'leftElbow', 'leftWrist', 'rightHip', 'rightKnee', 'rightAnkle', 'leftHip', 'leftKnee', 'leftAnkle', 'Pelvis', 'Spine', 'nose', 'R_Hand', 'L_Hand', 'R_Toe', 'L_Toe')
 
-def now():
-    return datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+            np.int16, np.int32, np.int64, np.uint8,
+            np.uint16, np.uint32, np.uint64)):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+            return float(obj)
+        elif isinstance(obj,(np.ndarray,)): #### This is the fix
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
 
-@singledispatch
-def to_serializable(val):
-    """Used by default."""
-    return str(val)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Poser')
+    requiredNamed = parser.add_argument_group('required named arguments')
+    requiredNamed.add_argument('-v', '--video', dest='videoFilePath', help='video input file', required=True)
+    requiredNamed.add_argument('-o', '--output', dest='outputRecordingPath', help='output recording file', required=True)
+    args = parser.parse_args()
 
-image = cv2.imread('/home/levishai_g/pose_estimation/images/input.jpg')
+    recordingFile = open(args.outputRecordingPath, 'w')
+    # image = cv2.imread('/home/levishai_g/pose_estimation/images/input.jpg')
+    cap = cv2.VideoCapture(args.videoFilePath)
+    videoFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    currentFrame = 0
 
-bbox_list = [
-[162.12181091308594, 86.23428344726562, 203.85377502441406, 255.6412353515625],\
-[286.82415771484375, 64.6770248413086, 74.55291748046875, 160.64566040039062],\
-[539.04931640625, 49.81459045410156, 99.95068359375, 233.66294860839844],\
-[365.950439453125, 170.007080078125, 270.9305419921875, 219.5510864257812],\
-[0.7226638793945312, 44.89130401611328, 85.53158569335938, 221.61691284179688]]
+    detectron = Detectron()
+    posenet = Posenet()
 
-rootnet_data = {
-        'bbox_list': bbox_list,
-        'image' : codecs.encode(pickle.dumps(image), "base64").decode() # protocol 0 is printable ASCII
-        }
-response = requests.post("http://localhost:3000", data=json.dumps(rootnet_data))
-root_depth_list = json.loads(response.text)['root_depth_list']
+    while cap.isOpened():
+        ret, image = cap.read()
+        if ret:
+            currentFrame = currentFrame + 1
+        else:
+            print(f'failed to extract frame {currentFrame} from the video')
+            break
 
-posenet = Posenet()
-poses_3d = posenet.process_image(image, bbox_list, root_depth_list)
-poses = []
+        print(f'===== Processing frame # {currentFrame} / {videoFrames} =====')
 
-for pose in poses_3d:
-   # print(pose)
-   keypoints = []
-   for i, joint in enumerate(pose):
-       keypoint = {
-           'score': 1,
-           'part': joints_name2[i],
-           'position' : {
-               'x': joint[0],
-               'y': joint[1],
-               'z': joint[2],
-           }
-       }
-       keypoints.append(keypoint)
-       if joints_name2[i] == 'nose':
-           keypoint = keypoint.copy()
-           keypoint['part'] = 'leftEye'
-           keypoints.append(keypoint)
-           keypoint = keypoint.copy()
-           keypoint['part'] = 'rightEye'
-           keypoints.append(keypoint)
-           keypoint = keypoint.copy()
-           keypoint['part'] = 'leftEar'
-           keypoints.append(keypoint)
-           keypoint = keypoint.copy()
-           keypoint['part'] = 'rightEar'
-           keypoints.append(keypoint)
+        print('detectron running')
+        bbox_list = detectron.process_image(image)
+        print('rootnet running')
+        rootnet_request_data = {
+            'bbox_list': bbox_list,
+            'image' : codecs.encode(pickle.dumps(image), "base64").decode() # protocol 0 is printable ASCII
+            }
+        response = requests.post("http://localhost:3000", data=json.dumps(rootnet_request_data, cls=NumpyEncoder))
+        root_depth_list = json.loads(response.text)['root_depth_list']
 
-   poses.append({'score': 1, 'keypoints': keypoints})
+        print('posenet running')
+        poses_3d = posenet.process_image(image, bbox_list, root_depth_list)
+        poses = []
 
-print(json.dumps({'timestamp': 0, 'poses': poses }, default=to_serializable))
+        for pose in poses_3d:
+            # print(pose)
+            keypoints = []
+            for i, joint in enumerate(pose):
+                keypoint = {
+                    'score': 1,
+                    'part': joints_name2[i],
+                    'position' : {
+                    'x': joint[0],
+                    'y': joint[1],
+                    'z': joint[2],
+                    }
+                }
+                keypoints.append(keypoint)
+                if joints_name2[i] == 'nose':
+                    keypoint = keypoint.copy()
+                    keypoint['part'] = 'leftEye'
+                    keypoints.append(keypoint)
+                    keypoint = keypoint.copy()
+                    keypoint['part'] = 'rightEye'
+                    keypoints.append(keypoint)
+                    keypoint = keypoint.copy()
+                    keypoint['part'] = 'leftEar'
+                    keypoints.append(keypoint)
+                    keypoint = keypoint.copy()
+                    keypoint['part'] = 'rightEar'
+                    keypoints.append(keypoint)
+
+            poses.append({'score': 1, 'keypoints': keypoints})
+
+        recordingFile.write(json.dumps({
+                'timestamp': int(cap.get(cv2.CAP_PROP_POS_MSEC)),
+                'poses': poses },
+            cls=NumpyEncoder))
+        recordingFile.write('\n')
+
+    cap.release()
+    recordingFile.close()
